3141450b5867068752cf146e9735535d0b00cf23
Diegomoro415 <diego_moro415@hotmail.com>
2023-03-18 21:44:46 +0000

    import forex-python and create instances of currencies classes

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import Library
+from forex_python.converter import CurrencyRates, CurrencyCodes
+
+# Create instance of CurrencyRates class
+cr = CurrencyRates()
+
+# Create instance of CurrencyCodes class
+cc = CurrencyCodes()
