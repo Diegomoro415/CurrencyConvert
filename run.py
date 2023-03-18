45b66301da228208600e6f24947d760bc8e32173
Diegomoro415 <diego_moro415@hotmail.com>
2023-03-18 21:49:56 +0000

    add a list of currencies and a function with the top currencies

--- run.py
+++ run.py
@@ -6,3 +6,20 @@ cr = CurrencyRates()
 
 # Create instance of CurrencyCodes class
 cc = CurrencyCodes()
+
+# Define available currencies
+currencies = ['USD', 'EUR', 'GBP', 'JPY', 'CAD', 'AUD',
+              'CHF', 'NZD', 'CNY', 'HKD', 'SGD', 'MXN',
+              'SEK', 'NOK', 'DKK', 'TRY', 'ZAR', 'RUB',
+              'INR', 'BRL', 'ARS', 'EGP', 'KRW', 'MYR',
+              'THB', 'PHP', 'CLP', 'COP', 'IDR', 'NGN',
+              'AED', 'BGN', 'HRK', 'CZK', 'HUF', 'ILS',
+              'KWD', 'RON', 'SAR', 'VND', 'BTC', 'ETH',
+              'BNB', 'ADA', 'XRP', 'SOL', 'DOT', 'DOGE',
+              'UNI', 'LTC', 'MATIC', 'LINK', 'XLM', 'VET',
+              'ATOM', 'AVAX', 'ALGO', 'FIL', 'TRX', 'BCH']
+
+
+def top_currencies():
+    print('\033[32m\n1-USD\n2-EUR\n3-GBP\n4-JPY\n5-CNY\n6-AUD\n7-HKD\n8-CAD\n'
+          '9-BTC\n0-Others:\n\33[0;0m')
