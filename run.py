3744d5fb3a36f6045259b693d0e169232c540527
Diegomoro415 <diego_moro415@hotmail.com>
2023-03-19 12:22:00 +0000

    install forex-python again and fix code position

--- run.py
+++ run.py
@@ -119,77 +119,77 @@ while True:
             print(
                 "\033[31m Input not valid!.  Choose a valid value :(\33[0;0m")
 
-        # Select a currency to exchage value
-        label('SELECT THE CONVERSION CURRENCY')
-        currency_to = None
-        while currency_to not in (1, 2, 3, 4, 5, 6, 7, 8, 9, 0):
-            top_currencies()
-            try:
-                currency_to = int(input("Select a currency:  "))
-                if currency_to == 1:
-                    currency_to = 'USD'
-                    print("code chosen:", currency_to)
-                    break
-                elif currency_to == 2:
-                    currency_to = 'EUR'
-                    print("code chosen:", currency_to)
-                    break
-                elif currency_to == 3:
-                    currency_to = 'GBP'
-                    print("code chosen:", currency_to)
-                    break
-                elif currency_to == 4:
-                    currency_to = 'JPY'
-                    print("code chosen:", currency_to)
-                    break
-                elif currency_to == 5:
-                    currency_to = 'CNY'
-                    print("code chosen:", currency_to)
-                    break
-                elif currency_to == 6:
-                    currency_to = 'AUD'
-                    print("code chosen:", currency_to)
-                    break
-                elif currency_to == 7:
-                    currency_to = 'HKD'
-                    print("code chosen:", currency_to)
-                    break
-                elif currency_to == 8:
-                    currency_to = 'CAD'
-                    print("code chosen:", currency_to)
-                    break
-                elif currency_to == 9:
-                    currency_to = 'BTC'
-                    print("code chosen:", currency_to)
-                    break
-                elif currency_to == 0:
-                    currency_to = input(
-                        '\nOthers, insert currency code: (3 digits):').upper()
-                    print('Currency Code :', currency_to)
-                    break
-            except Exception:
-                print(
-                    "\033[31m Input not valid!. Try again :(\33[0;0m")
-                # Check if user choose currencies are available
-                if currency_from or currency_to not in currencies:
-                    print('Invalid input. The available currencies are: ')
-                    print(currencies)
-                    continue
-        # Conversion
-        label('CONVERTING...')
-        # Get the exchange rate between currencies
-        result = cr.get_rate(currency_from, currency_to, amount)
-        # Get the exchange rate between currencies
-        exchange_rate = cr.get_rate(currency_from, currency_to)
-        # Format the output
-        symbol_from = cc.get_symbol(currency_from)
-        symbol_to = cc.get_symbol(currency_to)
-        result = round(result, 2)
-        print(f'{currency_from}  -  {symbol_from} {amount} '
-              f' =========================== '
-              f' {currency_to}  -  {symbol_to} { result} '
-              f' Exchange rate: {exchange_rate:.4f})')
-        # Ask if user wants to perform another conversion
-        answer = input('Do you wish to perform another time? (Y/N): ').upper
-        if answer == 'N':
-            break
+    # Select a currency to exchage value
+    label('SELECT THE CONVERSION CURRENCY')
+    currency_to = None
+    while currency_to not in (1, 2, 3, 4, 5, 6, 7, 8, 9, 0):
+        top_currencies()
+        try:
+            currency_to = int(input("Select a currency:  "))
+            if currency_to == 1:
+                currency_to = 'USD'
+                print("code chosen:", currency_to)
+                break
+            elif currency_to == 2:
+                currency_to = 'EUR'
+                print("code chosen:", currency_to)
+                break
+            elif currency_to == 3:
+                currency_to = 'GBP'
+                print("code chosen:", currency_to)
+                break
+            elif currency_to == 4:
+                currency_to = 'JPY'
+                print("code chosen:", currency_to)
+                break
+            elif currency_to == 5:
+                currency_to = 'CNY'
+                print("code chosen:", currency_to)
+                break
+            elif currency_to == 6:
+                currency_to = 'AUD'
+                print("code chosen:", currency_to)
+                break
+            elif currency_to == 7:
+                currency_to = 'HKD'
+                print("code chosen:", currency_to)
+                break
+            elif currency_to == 8:
+                currency_to = 'CAD'
+                print("code chosen:", currency_to)
+                break
+            elif currency_to == 9:
+                currency_to = 'BTC'
+                print("code chosen:", currency_to)
+                break
+            elif currency_to == 0:
+                currency_to = input(
+                    '\nOthers, insert currency code: (3 digits):').upper()
+                print('Currency Code :', currency_to)
+                break
+        except Exception:
+            print(
+                "\033[31m Input not valid!. Try again :(\33[0;0m")
+            # Check if user choose currencies are available
+            if currency_from or currency_to not in currencies:
+                print('Invalid input. The available currencies are: ')
+                print(currencies)
+                continue
+    # Conversion
+    label('CONVERTING...')
+    # Get the exchange rate between currencies
+    result = cr.get_rate(currency_from, currency_to, amount)
+    # Get the exchange rate between currencies
+    exchange_rate = cr.get_rate(currency_from, currency_to)
+    # Format the output
+    symbol_from = cc.get_symbol(currency_from)
+    symbol_to = cc.get_symbol(currency_to)
+    result = round(result, 2)
+    print(f'{currency_from}  -  {symbol_from} {amount} '
+          f' =========================== '
+          f' {currency_to}  -  {symbol_to} { result} '
+          f' Exchange rate: {exchange_rate:.4f})')
+    # Ask if user wants to perform another conversion
+    answer = input('Do you wish to perform another time? (Y/N): ').upper
+    if answer == 'N':
+        break
